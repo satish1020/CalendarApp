0e334d7c42bebbc3937a4949f5c2be5171b5dad3
Satish Chakkirala <cvsatish.net@example.com>
2024-08-23 22:53:09 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CalendarApp from './CalendarApp'
+import './CalendarApp.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp />
+    </div>
+  )
+}
+
+export default App
